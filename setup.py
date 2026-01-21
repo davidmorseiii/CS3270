9dd94395686d07b9dca239ddc902f2cb5334afd2
davidmorseiii <dmmorse3@gmail.com>
2026-01-20 18:51:23 -0700

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CS3270',
+    version='0.1',
+    packages=find_packages(),
+    description='Simple tool for analyzing weather data CSV',
+    author='David Morse',
+    author_email='dmmorse3@gmail.com',
+    url='https://github.com/davidmorseiii/CS3270.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
