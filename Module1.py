60b6e6704c24e11836a3a4870547ff96d7507c5f
davidmorseiii <dmmorse3@gmail.com>
2026-01-20 18:34:18 -0700

    Updated load_weather_data() to include additional error handling. Removed print function in preparation for module 2.

--- Module1.py
+++ Module1.py
@@ -1,13 +1,17 @@
 import pandas as pd
+import os
 
 def load_weather_data(file_path):
     """
     Loads weather data from csv and returns a list of dictionaries.
     """
+    if not os.path.exists(file_path):
+        print(f"Error: file not found.")
+        return []
+
     try:
         # load raw data
         df = pd.read_csv(file_path)
-
         # check if file is empty
         if df.empty:
             print("Error: the csv file is empty.")
@@ -15,24 +19,13 @@ def load_weather_data(file_path):
 
         # convert dataframe to a list of dicts
         # I'm not very familiar with pandas so this seems like an easy way to handle right now
-        weather_list = df.to_dict(orient='records')
-
-        return weather_list
+        return df.to_dict(orient='records')
 
     except Exception as e:
-        print(f"Error: Something went wrong.")
+        print(f"Error reading file: {e}")
         return []
-
-def print_weather_summary(weather_data):
-    """
-    Prints the total number of rows found in the weather data csv.
-    """
-    row_count = len(weather_data)
-    print(f"--- Weather Data Summary ---")
-    print(f"Total records found: {row_count}")
 
 if __name__ == '__main__':
     file_name = "AustraliaWeatherData/Weather Training Data.csv"
 
     data = load_weather_data(file_name)
-    print_weather_summary(data)
